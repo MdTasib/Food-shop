ae868397c20a7052ae5d163f53073640d5dc82c1
MdTasib <ohidulalam442@gmail.com>
2022-03-26 10:19:46 +0600

    fake data createed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [foods, setFoods] = useState([]);
+
+	useEffect(() => {
+		fetch("data.json")
+			.then(res => res.json())
+			.then(data => setFoods(data));
+	}, []);
+
+	console.log(foods);
+
+	return (
+		<div className='App'>
+			{foods.map(food => (
+				<>
+					<h4>{food.name}</h4>
+					<img src={food.img} alt='' />
+				</>
+			))}
+		</div>
+	);
 }
 
 export default App;
